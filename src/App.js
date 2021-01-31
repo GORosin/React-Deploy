592168477452683b088ffe919c08d75a30927757
GORosin <oxr3231@g.rit.edu>
2021-01-30 19:25:56 -0500

    added background image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import bg from './bg.jpg';
 
 function App() {
   return (
     <div className="App">
-      
+      <img src = {bg} alt = "background"/>
     </div>
   );
 }
